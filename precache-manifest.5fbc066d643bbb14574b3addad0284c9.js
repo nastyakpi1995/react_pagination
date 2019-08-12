37af35c78d67eec33c021e30e8263024235f65c7
Nastia Krat <nastyakpi1995@gmail.com>
2019-08-12 21:34:39 +0300

    Updates

---
+++ precache-manifest.5fbc066d643bbb14574b3addad0284c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70f29d17687f0102f56b936bf8a44b6",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "31da976534bf6c4e12dd",
+    "url": "/react_pagination/static/css/main.4004b58e.chunk.css"
+  },
+  {
+    "revision": "37b4d4179c77611cb2d7",
+    "url": "/react_pagination/static/js/2.743532c1.chunk.js"
+  },
+  {
+    "revision": "31da976534bf6c4e12dd",
+    "url": "/react_pagination/static/js/main.dd5573dd.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_pagination/precache-manifest.18d157d311a4914aee9cc28751f3f8a7.js"
+  "/react_pagination/precache-manifest.5fbc066d643bbb14574b3addad0284c9.js"
 );
 
 self.addEventListener('message', (event) => {
